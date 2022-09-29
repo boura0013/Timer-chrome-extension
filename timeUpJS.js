8f7c0ed7a2a7c5e7556410284df1e7a1d24cfeec
boura0013 <bourdeaux.adrien@gmail.com>
2022-09-29 08:41:20 -0400

    Added a page that opens to signal timer end

--- timeUpJS.js
+++ timeUpJS.js
@@ -1,2 +1,6 @@
-// Play the audio
-('#playAudio').remove() // just to make sure that it will not have 2x audio in the background
+// Play the auio
+document.getElementById("playAudioButton").onclick = function(){
+    console.log("button clicked");
+    var audio = new Audio("https://drive.google.com/uc?export=download&id=1hPPyIayYAuEr2cHuR9n5G6c_khDbEIvt");
+    audio.play();
+}
